e578314770359e02212af8e873b02ef4d10869e0
jlareck <medik2507@gmail.com>
2020-10-08 23:40:22 +0300

    created tests

---
+++ OOP/airline/src/test/airlinetest/airline/AirplaneTest.java
@@ -0,0 +1,21 @@
+package airline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AirplaneTest {
+    @Test
+    void testAirplane(){
+        Aircraft airplane = new Airplane("airplane",10,20, 30);
+        assertEquals(airplane.getFuel(), 30);
+        assertEquals(airplane.getPayload(), 10);
+        assertEquals(airplane.getRangeOfFlight(), 20);
+        airplane.setFuel(15);
+        assertEquals(airplane.getFuel(),15);
+        airplane.setPayload(200);
+        assertEquals(airplane.getPayload(), 200);
+        airplane.setRangeOfFlight(500);
+        assertEquals(airplane.getRangeOfFlight(),500);
+    }
+}
---
+++ OOP/airline/src/test/airlinetest/airline/AirportTest.java
@@ -0,0 +1,37 @@
+package airline;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AirportTest {
+    @Test
+    void testAirport() {
+        Airport airport = new Airport("airport");
+        Aircraft airplaneA = new Airplane("A", 10,20,20);
+        Aircraft airplaneB = new Airplane("B", 10,100,100);
+        Aircraft airplaneC = new Airplane("C", 10,70,70);
+        Aircraft airplaneD = new Airplane("A", 10,10,30);
+
+        airport.addAircraft(airplaneA);
+        airport.addAircraft(airplaneB);
+        airport.addAircraft(airplaneC);
+        airport.addAircraft(airplaneD);
+        airport.sortAircraftsByRangeOfFlight();
+
+        Aircraft arr[] = new Aircraft[]{airplaneD,airplaneA,airplaneC,airplaneB};
+        List<Aircraft> expectedSortedAircrafts = new ArrayList<>(Arrays.asList(arr));
+
+        assertEquals(airport.getAllAircrafts(), expectedSortedAircrafts);
+
+        List<Aircraft> expecteFoundAircrafts = new ArrayList<>();
+        expecteFoundAircrafts.add(airplaneC);
+        expecteFoundAircrafts.add(airplaneB);
+        assertEquals(airport.getAircraftsByFuelRange(50, 150), expecteFoundAircrafts);
+
+    }
+}
