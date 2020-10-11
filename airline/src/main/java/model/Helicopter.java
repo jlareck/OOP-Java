593fac4adf1bc9566835b011ed031f10108a7839
jlareck <medik2507@gmail.com>
2020-10-11 16:45:42 +0300

    updated project structure

---
+++ airline/src/main/java/model/Helicopter.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Helicopter extends Aircraft {
+    public Helicopter(String name, int payload, int rangeOfFlight, int fuel) {
+        super(name, payload, rangeOfFlight, fuel);
+    }
+}
