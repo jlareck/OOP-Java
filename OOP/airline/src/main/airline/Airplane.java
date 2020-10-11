a9bc2d1ecdfe055bc1303ff35e88c6574e2315d5
jlareck <medik2507@gmail.com>
2020-10-11 16:16:34 +0300

    deleted project

--- OOP/airline/src/main/airline/Airplane.java
+++
@@ -1,8 +0,0 @@
-package airline;
-
-public class Airplane extends Aircraft{
-
-    public Airplane(String name, int payload, int rangeOfFlight, int fuel) {
-        super(name, payload, rangeOfFlight, fuel);
-    }
-}
--- OOP/airline/src/main/airline/Helicopter.java
+++
@@ -1,7 +0,0 @@
-package airline;
-
-public class Helicopter extends Aircraft {
-    public Helicopter(String name, int payload, int rangeOfFlight, int fuel) {
-        super(name, payload, rangeOfFlight, fuel);
-    }
-}
